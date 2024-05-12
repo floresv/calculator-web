d7a97ec72815cf5ef7dd223f0a08523a11f0f96c
Javier Flores <jflores@MacBook-Pro-3.local>
2024-05-11 23:52:31 -0400

    add base views

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,72 @@
-<script setup lang="ts">
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
-
 <template>
-  <div class="item">
-    <div class="details">
-      <h3>
-        <slot name="heading">New Operation</slot>
-      </h3>
-      Addition
-    </div>
-  </div>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="firstname"
+            :counter="10"
+            :rules="nameRules"
+            label="First name"
+            hide-details
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="lastname"
+            :counter="10"
+            :rules="nameRules"
+            label="Last name"
+            hide-details
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-select
+            v-model="select"
+            :items="items"
+            :rules="[(v) => !!v || 'Item is required']"
+            label="Item"
+            required
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row class="align-center">
+        <v-col cols="12" md="4"></v-col>
+        <v-col cols="12" md="4">
+          <v-btn class="mt-4" color="success" block @click="calculate"> Calculate </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
+<script lang="ts">
+export default {
+  data: () => ({
+    valid: false,
+    firstname: '',
+    lastname: '',
+    nameRules: [
+      (value) => {
+        if (value) return true
+
+        return 'Name is required.'
+      },
+      (value) => {
+        if (value?.length <= 10) return true
+
+        return 'Name must be less than 10 characters.'
+      }
+    ]
+  }),
+
+  methods: {
+    async calculate() {
+      console.log(lastname + 'logged in')
+    }
+  }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { VApp } from 'vuetify/components'
 
 loadFonts()
 
